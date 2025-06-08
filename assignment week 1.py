63fd09329b26e724edc2de83b71686f7213e1fe2
Kartik360saini <kartiksaini360@gmail.com>
2025-06-08 09:49:21 +0530

    assignment week 1.py

--- assignment week 1.py
+++ assignment week 1.py
@@ -1,23 +1,20 @@
-# lower triangular pattern 
+#rows
+x=5
 
-x=int(input("enter a number till where you want a triangle: "))
+# lower triangular pattern 
+print("LOWER TRIANGLE")
 for i in range(x+1):
     print("*"*i)
-    print()
 
 
 # upper triangular pattern
-
-x=int(input("enter a number till where you want a triangle: "))
+print("UPPER TRIANGLE")
 for i in range(x+1):
     print(" "*(x-i),end="")
     print("*"*i)
-    print()
 
-# pyramid pattern 
-
-x=int(input("enter a number till where you want a triangle: "))
+# pyramid triangle pattern 
+print("PYRAMID TRIANGLE")
 for i in range(x+1):
     print(" "*(x-i),end="")
     print("*"*(2*i-1))
-    print()
